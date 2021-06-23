a2b7be4a74b51f6bc25c3658cfae3c7407109900
Pramit Ghosh <pramitghosh@outlook.com>
2021-06-23 11:53:28 +0200

    correct indentation issues in JS

--- selectdeselect.js
+++ selectdeselect.js
@@ -1,19 +1,19 @@
+function selects(cat_name)
+{  
+	var ele=document.getElementsByName(cat_name);  
+	for(var i=0; i<ele.length; i++)
+    {  
+		if(ele[i].type=='checkbox')  
+			ele[i].checked=true;  
+    }  
+}
 
-            		function selects(cat_name)
-            		{  
-	                	var ele=document.getElementsByName(cat_name);  
-	                	for(var i=0; i<ele.length; i++)
-                		{  
-		                    if(ele[i].type=='checkbox')  
-		                    ele[i].checked=true;  
-                		}  
-            		}  
-            		function deSelect(cat_name)
-            		{  
-                		var ele=document.getElementsByName(cat_name);  
-                		for(var i=0; i<ele.length; i++)
-                		{  
-                    		if(ele[i].type=='checkbox')  
-                        	ele[i].checked=false;  
-                        }
-                    }             
+function deSelect(cat_name)
+{  
+	var ele=document.getElementsByName(cat_name);  
+    for(var i=0; i<ele.length; i++)
+    {  
+    	if(ele[i].type=='checkbox')  
+        	ele[i].checked=false;  
+    }
+}             
