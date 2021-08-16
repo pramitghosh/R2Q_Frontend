b76e4af3a0981dbdb32f07be99df8ebe68daeeb3
Flemming Albers <fl-albers@web.de>
2021-08-16 22:29:30 +0200

    pdf content added

--- mpdf.php
+++ mpdf.php
@@ -11,17 +11,28 @@ $stylesheet = file_get_contents('pdf_style.css');
 
 $mpdf->WriteHTML($stylesheet,\Mpdf\HTMLParserMode::HEADER_CSS);
 // $mpdf->Image('startBackground.png', 0, 0, 210, 297, 'png', '', true, false);
-$mpdf->SetHTMLHeader("
-<div style='text-align: left; font-weight: bold;'>
 
-    <img class='Logo' src='R2Q_LogoNT.jpg'; >
+$mpdf->SetHTMLHeader("
+<div class='header'>
+Maßnahmenkatalog
+<div class='hlineHeader'></div>
 </div>");
-$mpdf->WriteHTML("<h1>Hello world!". $r_Umsetzungsbeispiel_Beschriftung ." </h1>
-<img class='figure_bsp' src='" . $IMGpath . "'; >");
 
 $mpdf->SetHTMLFooter("
 <div style='text-align: right; font-weight: bold;'>
-    <img class='figure_bsp' src='Logofooter.png'; >
+    <img class='figure_bsp' src='LogofooterNT.png'; >
 </div>");
+
+$mpdf->WriteHTML("<h3 style = 'width: 1000px; text-align: justify;'>" . $r_Titel . "</h3>
+    <h5>Kurzinformation</h5>
+	<div id='kurzBox' class='greenBox'> 
+		<p>"
+			. $r_Kurzbeschreibung . 
+		"</p>
+	</div>");
+
+$mpdf->WriteHTML("<img class='figure_bsp' src='" . $IMGpath . "'; >");
+
+
 
 $mpdf->Output("mpdf.pdf", "I"); // für direkten Download der PDF -> "D" eingeben 
