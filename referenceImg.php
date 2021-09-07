69cf97c25944dfc728c43072abc80d5b721a1844
fa655601 <fa655601@fh-muenster.de>
2021-09-07 16:13:48 +0200

    Picture references added

--- referenceImg.php
+++ referenceImg.php
@@ -31,6 +31,31 @@
 				</div>
 				<div class = "refContent">
 					<h3  style = "width: 1000px; text-align: justify;"> Bildverweise </h3>
+					<table class="refTable" style="border-top: 2px solid black" >
+						<colgroup>
+							<col style='width:30%'>
+							<col style='width:70%'>
+						</colgroup>
+						<thead class='search'>
+							<td style='font-size: 25px;' >Bild &nbsp;</td>
+							<td style='font-size: 25px;' >Quelle &nbsp;</td>
+						</thead>
+						<tbody>
+							<tr class="refRow">
+								<td style="padding: 3px 5px">Hintergrundbild Startseite</td>
+								<td style="padding: 3px 5px">CHUTTERSNAP, 04.09.2017, <a href="https://unsplash.com/photos/oqJxJ4TYoQg"> https://unsplash.com/photos/oqJxJ4TYoQg</a> </td>
+							</tr>
+							<tr class="refRow">
+								<td style="padding: 3px 5px">Hintergrundbild Maßnahmenkatalog</td>
+								<td style="padding: 3px 5px">Danist Soh, 05.01.2016, <a href="https://unsplash.com/photos/dqXiw7nCb9Q"> https://unsplash.com/photos/dqXiw7nCb9Q</a> </td>
+							</tr>
+							<!-- <tr class="searchrow">
+								<td></td>
+								<td></td>
+								<td></td> -->
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 			<?php include 'footer.php'; ?>
